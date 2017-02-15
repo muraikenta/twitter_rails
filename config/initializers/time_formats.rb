8f05cd6ea9f4b0a5839b8fefad835722530c995d
村井謙太 <muraikenta0507@gmail.com>
2017-02-15 15:16:35 +0900

    Set time format

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,8 @@
+Time::DATE_FORMATS[:default] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:datetime] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:date] = '%Y/%m/%d'
+Time::DATE_FORMATS[:date_dot] = '%Y.%m.%d'
+Time::DATE_FORMATS[:time] = '%H:%M:%S'
+Time::DATE_FORMATS[:month] = '%-m月%-d日 %H:%M'
+Time::DATE_FORMATS[:date_jp] = '%Y年%m月%d日'
+Date::DATE_FORMATS[:date_jp] = '%Y年%m月%d日'
