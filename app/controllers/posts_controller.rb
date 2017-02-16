3c8163376e385260f26b30c5987961fc9fcbf4f5
村井謙太 <muraikenta0507@gmail.com>
2017-02-16 17:30:31 +0900

    Lesson7 prototype

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -7,6 +8,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = @post.user
   end
 
   def new
@@ -14,7 +16,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(content: params[:content], user_id: @current_user.id)
     if @post.save
       flash[:notice] = '投稿を作成しました'
       redirect_to '/posts/index'
@@ -32,7 +34,7 @@ class PostsController < ApplicationController
     @post.content = params[:content]
     if @post.save
       flash[:notice] = '投稿を編集しました'
-      redirect_to '/posts/index'
+      redirect_to "/posts/#{@post.id}"
     else
       render 'edit'
     end
@@ -43,5 +45,15 @@ class PostsController < ApplicationController
     @post.destroy
     flash[:notice] = '投稿を削除しました'
     redirect_to '/posts/index'
+  end
+
+  private
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @current_user.id != @post.user_id
+      flash[:notice] = '権限がありません'
+      redirect_to '/posts/index'
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    @posts = @user.posts
   end
 
   def new
