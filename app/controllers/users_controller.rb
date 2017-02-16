7ce5b3314166be276324f3377d6be34cc8a054d5
村井謙太 <muraikenta0507@gmail.com>
2017-02-16 15:00:45 +0900

    Lesson6 prototype

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:edit_profile, :update_profile, :edit_password, :update_password]
+  before_action :forbid_login_user, only: [:new, :create, :login_form, :login]
+  
   def show
     @user = User.find_by(id: params[:id])
   end
@@ -8,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:title], email: params[:email], password: params[:password])
+    @user = User.new(name: params[:title], email: params[:email], password: BCrypt::Password.create(params[:password]))
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = 'ユーザー登録が完了しました'
@@ -18,13 +21,42 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit_profile
+  end
+
+  def update_profile
+    @current_user.name = params[:name]
+    @current_user.email = params[:email]
+    if @current_user.save
+      flash[:notice] = 'アカウント情報を編集しました'
+      redirect_to "/users/#{@current_user.id}"
+    else
+      render 'edit_profile'
+    end
+  end
+
+  def edit_password
+  end
+
+  def update_password
+    if BCrypt::Password.new(@current_user.password).is_password?(params[:current_password])
+      @current_user.password = BCrypt::Password.create(params[:new_password])
+      @current_user.save
+      flash[:notice] = 'パスワードを変更しました'
+      redirect_to "/users/#{@current_user.id}"
+    else
+      @error_message = 'パスワードが違います'
+      render 'edit_password'
+    end
+  end
+
   def login_form
     @user = User.new
   end
 
   def login
     @user = User.find_by(email: params[:email])
-    if @user && @user.password == params[:password]
+    if @user && BCrypt::Password.new(@user.password).is_password?(params[:password])
       session[:user_id] = @user.id
       flash[:notice] = 'ログインしました'
       redirect_to "/users/#{@user.id}"
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'login' => 'users#login_form'
   get 'signup' => 'users#new'
+  get 'edit_profile' => 'users#edit_profile'
+  get 'edit_password' => 'users#edit_password'
   get 'users/:id' => 'users#show'
   post 'users/create' => 'users#create'
   post 'login' => 'users#login'
   post 'logout' => 'users#logout'
+  post 'update_profile' => 'users#update_profile'
+  post 'update_password' => 'users#update_password'
 
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
