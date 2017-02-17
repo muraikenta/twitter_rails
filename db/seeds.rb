4e7cfc3c486690ea0319b05bb8f9b7814a951bd4
村井謙太 <muraikenta0507@gmail.com>
2017-02-17 12:45:30 +0900

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.find_or_create_by(email: 'info@prog-8.com') do |new_user|
+  new_user.name = 'ninjwanko'
+  new_user.image = '/images/default_user.jpg'
+  new_user.password = BCrypt::Password.create('password')
+end
+
+post = user.posts.create!([
+  {
+    content: 'Railsコースがより分かりやすく進化する予定です！',
+  },
+  {
+    content: '事業が伸びすぎていて人が足りません。。。インスタ使ったプロモーション事業の営業とマーケで、興味があるかたDMください。 ほんとにください！',
+  },
+  {
+    content: 'プログラミング奨学金にエントリーしてくれた大学生に会ってみたら、即日で投資先スタートアップでフルタイムインターン決まった！プログラミング学ぶ人が増えるのもいいけどスタートアップに来る人が増えるのもいいことだ。',
+  },
+  {
+    content: '「第1回Progate Sunday School 」始まりました！！',
+  },
+])
